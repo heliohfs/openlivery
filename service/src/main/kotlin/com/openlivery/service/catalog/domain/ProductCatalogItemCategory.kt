28d45022c5551886185d993c605f1b4a43c339aa
Hélio <helio_hfs@hotmail.com>
2021-05-24 13:45:46 -0400

    chore: ProductCatalogItemCategory inherits from BaseEntity

--- service/src/main/kotlin/com/openlivery/service/catalog/domain/ProductCatalogItemCategory.kt
+++ service/src/main/kotlin/com/openlivery/service/catalog/domain/ProductCatalogItemCategory.kt
@@ -1,6 +1,6 @@
 package com.openlivery.service.catalog.domain
 
-import com.openlivery.service.common.entities.BaseEntity
+import com.openlivery.service.common.domain.BaseEntity
 import org.hibernate.annotations.Immutable
 import javax.persistence.*
 
